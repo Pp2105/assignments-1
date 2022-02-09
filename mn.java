d8aa486880a2889f1bc096f1a50f0f16bdeceac2
Prathamesh <pawarp212000@gmail.com>
2022-02-09 14:49:19 +0530

    'ff'

---
+++ mn.java
@@ -0,0 +1,32 @@
+package com.mphasis.classDemo;
+
+public class GetSetDemo {
+	
+		private String name;
+		private int empId;
+		private float salary;
+	
+	public String getName () {
+		return name;
+	}
+		
+	public void setName (String name) {
+		this.name = name;
+	}
+	
+	public int getEmpId() {
+		return empId;
+	}
+	
+	public void setEmpId (int EmpId) {
+		this.empId = empId;
+	}
+	
+		public float getSalary() {
+			return salary;
+		}
+		
+		public void setSalary (float Salary) {
+			this.salary = salary;
+		}
+}
